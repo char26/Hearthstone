52810f682a1bb4b59f0c2feb53f885fbba849fc3
Charlie Alders <charliealders26@gmail.com>
2025-01-05 20:59:29 -0800

    using map to differentiate player hearth locations

--- src/main/java/com/chartle/hearthstonemod/item/custom/HearthstoneItem.java
+++ src/main/java/com/chartle/hearthstonemod/item/custom/HearthstoneItem.java
@@ -3,6 +3,7 @@ package com.chartle.hearthstonemod.item.custom;
 import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.server.level.ServerLevel;
+import net.minecraft.util.Tuple;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
@@ -11,11 +12,13 @@ import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.phys.Vec3;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class HearthstoneItem extends Item {
-    private Vec3 boundVec = null;
-    private ResourceKey<Level> boundDimension = null;
+    private Map<Integer, Tuple<Vec3, ResourceKey<Level>>> playerIdToHearthstoneInfo = null;
     // TODO: add cast time
 
     public HearthstoneItem(Properties pProperties) {
@@ -24,25 +27,28 @@ public class HearthstoneItem extends Item {
 
     @Override
     public InteractionResult use(Level pLevel, Player pPlayer, InteractionHand pHand) {
-
         if (!pLevel.isClientSide()) {
+            if (playerIdToHearthstoneInfo == null) {playerIdToHearthstoneInfo = new HashMap<>();}
             ServerLevel serverLevel = (ServerLevel) pLevel;
             if (pPlayer.isCrouching()) {
-                boundDimension = serverLevel.dimension();
-                boundVec = new Vec3(pPlayer.getX(), pPlayer.getY(), pPlayer.getZ());
+                ResourceKey<Level> boundDimension = serverLevel.dimension();
+                Vec3 boundVec = new Vec3(pPlayer.getX(), pPlayer.getY(), pPlayer.getZ());
+                Tuple<Vec3, ResourceKey<Level>> hearthstoneInfo = new Tuple<>(boundVec, boundDimension);
+                playerIdToHearthstoneInfo.put(pPlayer.getId(), hearthstoneInfo);
                 pPlayer.displayClientMessage(Component.literal("Hearthstone location saved."), false);
                 return InteractionResult.SUCCESS;
             } else {
-                if (boundVec == null) {
+                Tuple<Vec3, ResourceKey<Level>> hearthstoneInfo = playerIdToHearthstoneInfo.get(pPlayer.getId());
+                if (hearthstoneInfo == null) {
                     pPlayer.displayClientMessage(Component.literal("Hearthstone is not set. Crouch and right-click to set."), false);
                     return InteractionResult.PASS;
                 }
-
                 // TODO: allow teleporting to/from nether
-                if (serverLevel.dimension() != boundDimension) {
+                if (serverLevel.dimension() != hearthstoneInfo.getB()) {
                     pPlayer.displayClientMessage(Component.literal("Cannot teleport to another dimension. (Coming soon)"), false);
                     return InteractionResult.PASS;
                 }
+                Vec3 boundVec = hearthstoneInfo.getA();
                 pPlayer.teleportTo(boundVec.x, boundVec.y, boundVec.z);
                 // TODO: add sound effect
             }
